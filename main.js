ff1f99e23d40ae02225fe67b5512d33793af3083
LucaStellato <lucastellato435@gmail.com>
2026-02-09 15:46:38 +0100

    complete exercices

--- main.js
+++ main.js
@@ -14,3 +14,38 @@ Utilizzare Promise.all() per eseguire più richieste in parallelo.
 Restituire una Promise che risolve un oggetto contenente i dati aggregati.
 Stampare i dati in console in un messaggio ben formattato.
 Testa la funzione con la query "london"*/
+
+
+async function getDashboardData(query) {
+    const nameCountryPromises = fetch(`http://localhost:3333/destinations?search=${query}`)
+        .then(nameCountry => nameCountry.json())
+    const weatherPromises = fetch(`http://localhost:3333/weathers?search=${query}`)
+        .then(weather => weather.json())
+    const airportPromises = fetch(`http://localhost:3333/airports?search=${query}`)
+        .then(airport => airport.json())
+    const [nameCountry, weather, airport] = await Promise.all([nameCountryPromises, weatherPromises, airportPromises])
+
+    const data = {
+        country: nameCountry[0].country,
+        temperature: weather[0].temperature,
+        weather: weather[0].weather_description,
+        airport: airport[0].name
+    };
+
+
+    return data
+
+
+}
+
+getDashboardData('london')
+getDashboardData('london')
+    .then(data => {
+        console.log('Dasboard data:', data);
+        console.log(
+            `${data.name} is in ${data.country}.\n` +
+            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
+            `The main airport is ${data.airport}.\n`
+        );
+    })
+    .catch(error => console.error(error));
